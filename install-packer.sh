f6b0f173b1c1e60bfdb2ca1dd8f7758c0d2e74b6
Peter Sellars <psellars@gmail.com>
2020-04-01 17:25:40 +1300

    move packer to folder not dir

--- install-packer.sh
+++ install-packer.sh
@@ -7,4 +7,4 @@ curl -OL \
   "https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip"
 unzip ./packer_${PACKER_VERSION}_linux_amd64.zip
 chmod +x packer
-mv packer /usr/local/packer
+mv packer /usr/local/
